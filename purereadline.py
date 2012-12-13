8761563be005bf7c0ad1cead7eac6a48a63bd110
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2012-12-13 10:49:24 -0800

    add comments and read_init_file

--- purereadline.py
+++ purereadline.py
@@ -6,38 +6,53 @@
 from ctypes import *
 import copy
 
-libreadline = CDLL('libreadline.so')
-libhistory = CDLL('libhistory.so')
+# libreadline.so and libhistory.so must be on LD_LIBRARY_PATH
+libreadline = cdll.LoadLibrary('libreadline.so')
+libhistory = cdll.LoadLibrary('libhistory.so')
 
 
+# parse_and_bind(*args)
+# =====================
+# 1. PyArg_parseTuple with format: "s" converts python string or Unicode object
+#   to char * -- check args are string or Unicode and convert Unicode to string
+# 2. don't catch copy error, since it raises copy.error exception
+# 3. don't recast s to c_char_p since Python pass strings as char *
+    
 # Exported function to send one line to readline's init file parser
 
 def parse_and_bind(s):
- """parse_and_bind(string) -> None\n\
-Parse and execute single line of a readline init file.""" 
-    if type(s) not in [str, unicode]
-        return None
+    """
+    parse_and_bind(string) -> None
+    Parse and execute single line of a readline init file.
+    """ 
+    if type(s) not in [str, unicode]:
+        return
+    elif type(s) is unicode:
+        s = str(s)
     # Make a copy -- rl_parse_and_bind() modifies its argument
     # Bernard Herzog
     s_copy = copy.copy(s)
-    c_s_copy = c_char_p(s_copy)
-    libreadline.rl_parse_and_bind(c_s_copy)
-    return None
+    libreadline.rl_parse_and_bind(s_copy)
 
- # Exported function to parse a readline init file
 
-def read_init_file(s)
-{
-    char *s = NULL;
-    if (!PyArg_ParseTuple(args, "|z:read_init_file", &s))
-        return NULL;
-    errno = rl_read_init_file(s);
-    if (errno)
-        return PyErr_SetFromErrno(PyExc_IOError);
-    Py_RETURN_NONE;
-}
+# read_init_file(*args)
+# =====================
+# 1. PyArg_ParseTuple with format: "|z" converts python string, Unicode or None
+#   to char * or NULL -- check args and convert Unicode to string
+# 2. rl_read_init_file returns 0 on success, non-zero int on fail
 
-PyDoc_STRVAR(doc_read_init_file,
-"read_init_file([filename]) -> None\n\
-Parse a readline initialization file.\n\
-The default filename is the last filename used.");
+# Exported function to parse a readline init file
+
+def read_init_file(s):
+    """
+    read_init_file([filename]) -> None
+    Parse a readline initialization file.
+    The default filename is the last filename used.
+    """
+    if type(s) not in [str, unicode, None]:
+        return
+    elif type(s) is unicode:
+        s = str(s)
+    errno = libreadline.rl_read_init_file(s);
+    if errno:
+        raise IOError(2,'No such file or directory',s)
